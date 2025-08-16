f9001da81e7e24887cf076cc6e4d504a9b1c6fe0
anurag-nair16 <anuragnair1607.com>
2025-08-17 00:01:28 +0530

    bug fix

--- sw.js
+++ sw.js
@@ -1,12 +1,32 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('expense-tracker-v1').then((cache) => {
+    caches.open('expense-tracker-v2').then((cache) => {
       return cache.addAll([
         '/',
-        '/index.html'
+        '/index.html',
+        '/manifest.json',
+        '/icon-192x192.png',
+        '/icon-512x512.png'
       ]);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['expense-tracker-v2'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
@@ -15,4 +35,11 @@ self.addEventListener('fetch', (event) => {
       return response || fetch(event.request);
     })
   );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'CHECK_UPDATE') {
+    self.skipWaiting();
+    self.clients.claim();
+  }
 });
